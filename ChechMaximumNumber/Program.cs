18cfe22c5b6dab205cac5ea80d0984ab75cd0f81
nikita5677 <67727961+nikita5677@users.noreply.github.com>
2022-12-01 15:15:46 +0530

    Program Solution

--- ChechMaximumNumber/Program.cs
+++ ChechMaximumNumber/Program.cs
@@ -33,7 +33,7 @@ namespace ChechMaximumNumber
             //Console.WriteLine("Maximum Float Value:" + MaxOf3Values.findmax<float>(2.14f, 4.63f, 1.22f));
             //Console.WriteLine("Maximum String Value:" + MaxOf3Values.findmax<string>("aaa", "ccc", "bbb"));
 
-            ////Refactor 2
+            //Refactor 2
             //Console.WriteLine("Maximum Integer Is:" + MaxOfValues<int>.findmax(4, 5, 3));
             //Console.WriteLine("Maximum Float Is:" + MaxOfValues<float>.findmax(4.32f, 2.47f, 1.55f));
             //Console.WriteLine("Maximum String Is:" + MaxOfValues<string>.findmax("yyy", "xxx", "zzz"));
