3513cd4d10487030c8fce598f2b6932995bbb59e
nikit <nikita.nikita@gmail.com>
2022-12-01 12:05:59 +0530

    Add project files.

---
+++ ChechMaximumNumber/Refactor2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChechMaximumNumber
+{
+    class MaxOfValues<T> where T : IComparable<T>
+    {
+        public T first, second, third;
+
+        public MaxOfValues(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        public static T findmax(T first, T second, T third)
+        {
+            //If first-second>0 && first-third>0
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+            //If second-first>0 && second-third>0
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+            //If third-first>0 && third-second>0
+            if (third.CompareTo(second) > 0 && first.CompareTo(first) > 0 ||
+                third.CompareTo(second) >= 0 && first.CompareTo(first) > 0 ||
+                third.CompareTo(second) > 0 && first.CompareTo(first) >= 0)
+            {
+                return third;
+            }
+            return first;
+        }
+    }
+}
